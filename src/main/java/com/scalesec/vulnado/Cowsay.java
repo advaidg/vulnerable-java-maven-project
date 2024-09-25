cf3019cd7fc2115a00103d243d0cdadc16dc28fa
Advaid Gireesan <73230570+advaidg@users.noreply.github.com>
2024-09-25 23:33:41 +0530

    Update Cowsay.java

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -4,10 +4,13 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Cowsay {
+  private static ProcessBuilder processBuilder;  // Introduce a potential nullability issue with the field
+
   public static String run(String input) {
-    ProcessBuilder processBuilder = new ProcessBuilder();
+    processBuilder = new ProcessBuilder(); // Assign a value initially
     String cmd = "/usr/games/cowsay '" + input + "'";
     System.out.println(cmd);
+    
     processBuilder.command("bash", "-c", cmd);
 
     StringBuilder output = new StringBuilder();
@@ -20,9 +23,17 @@ public class Cowsay {
       while ((line = reader.readLine()) != null) {
         output.append(line + "\n");
       }
+
+      // Introduce a scenario where processBuilder is set to null
+      processBuilder = null;  // This will trigger a Sonar issue warning "Field processBuilder might become null"
+      
     } catch (Exception e) {
       e.printStackTrace();
     }
+
+    // Try to use processBuilder again after setting it to null, which would lead to a null pointer exception
+    processBuilder.command("bash", "-c", "echo 'This will fail'");
+
     return output.toString();
   }
 }
